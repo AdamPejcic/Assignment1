d08f7204b2fecf1317e3d28eea0633ab6852d4ca
Adam <22011604@massey.ac.nz>
2023-08-28 21:26:06 +1200

    Added menu bar, file and edit menus and about, exit, select all, copy, paste and cut menu items.

--- src/main/java/Notepad.java
+++ src/main/java/Notepad.java
@@ -6,8 +6,29 @@ public class Notepad {
         JFrame frame = new JFrame("Notepad");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
+
         JTextArea textArea = new JTextArea();
         frame.add(textArea, BorderLayout.CENTER); // Add the text area to the center of the frame
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenuItem aboutMenuItem = new JMenuItem("About");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        JMenuItem selectAllMenuItem = new JMenuItem("Select All");
+        JMenuItem copyMenuItem = new JMenuItem("Copy");
+        JMenuItem pasteMenuItem = new JMenuItem("Paste");
+        JMenuItem cutMenuItem = new JMenuItem("Cut");
+        fileMenu.add(aboutMenuItem);
+        fileMenu.add(exitMenuItem);
+        editMenu.add(selectAllMenuItem);
+        editMenu.add(copyMenuItem);
+        editMenu.add(pasteMenuItem);
+        editMenu.add(cutMenuItem);
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        frame.setJMenuBar(menuBar);
+
         frame.setVisible(true);
     }
 
